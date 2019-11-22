d74eb919b6fd719753f396d555d5f030bfb49f2b
Ulyana <ulyanakapeshina@gmail.com>
2019-11-22 21:17:06 +0300

    Updates

---
+++ precache-manifest.c94068a51d64b09669619d8a7cde9bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f693b04e894e7907592e8474e2c776",
+    "url": "/react-lesson1/index.html"
+  },
+  {
+    "revision": "297461061b4c33c56836",
+    "url": "/react-lesson1/static/css/main.e08e42dd.chunk.css"
+  },
+  {
+    "revision": "8e1776095e209a37f59b",
+    "url": "/react-lesson1/static/js/2.0fa39d4f.chunk.js"
+  },
+  {
+    "revision": "297461061b4c33c56836",
+    "url": "/react-lesson1/static/js/main.abca41f0.chunk.js"
+  },
+  {
+    "revision": "d9aec467585295a82132",
+    "url": "/react-lesson1/static/js/runtime-main.e3a5aee7.js"
+  }
+]);
